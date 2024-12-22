5f621028b4309c06d99cf03bec7a11d384a55677
keishi44 <studymansth44@gmail.com>
2024-12-22 15:00:07 +0630

    ChangeArraySizeInRuntime

---
+++ src/changeSizeofArrayWhileRuntime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int size;
+    cout << "Size : ";
+    cin >> size;
+
+    int* myArray = new int[size];
+
+    for(int i = 0; i < size; i++){
+        cout << "Array[" << i << "] : ";
+        cin >> *(myArray + i);
+    }
+
+    for(int i = 0; i < size; i++){
+        cout << myArray[i] << " ";
+    }
+
+}
