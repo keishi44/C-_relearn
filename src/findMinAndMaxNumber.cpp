44507063f95f3faec7207400e139875d6edfe0cc
keishi44 <studymansth44@gmail.com>
2024-12-22 14:36:55 +0630

    Update

--- src/findMinAndMaxNumber.cpp
+++ src/findMinAndMaxNumber.cpp
@@ -20,8 +20,22 @@ int getMax(int numbers[] , int size){
     return max;
 }
 
+void getMinAndMax(int numbers[] , int size , int *max , int *min){
+    for(int i = 1; i < size; i++){
+        if(numbers[i] > *max){
+            *max = numbers[i];
+        }
+        else if(numbers[i] < *min){
+            *min = numbers[i];
+        }
+    }
+}
+
 int main(){
     int numbers[5] = {4,8,-8,44,77};
-    std::cout << getMin(numbers,5) << std::endl;
-    std::cout << getMax(numbers,5) << std::endl;
+    int min = numbers[0];
+    int max = numbers[0];
+    getMinAndMax(numbers,5,&max,&min);
+    std::cout << "Min : " << min << std::endl;
+    std::cout << "Max : " << max << std::endl;
 }
