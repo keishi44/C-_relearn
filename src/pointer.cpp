f5d2aa54161a959ff918495ccfd7a8e3f90740b3
keishi44 <studymansth44@gmail.com>
2024-12-23 12:55:02 +0630

    update

--- src/pointer.cpp
+++ src/pointer.cpp
@@ -1,25 +1,10 @@
 #include <iostream>
 
-// many values from function return
 
-void print(void* ptr, char type){
-    switch(type){
-        case 'i':
-            std::cout << *((int*) ptr) << std::endl; // explicit
-            break;
-        case 'c':
-            std::cout << *((char*) ptr) << std::endl;
-            break;
-    }
-
-    // int x = 5.3;
-    // std::cout << (double)x; explicit
-}
 
 int main(){
     int number = 5;
     char letter = 'A';
-    print(&number ,'i');
-    print(&letter ,'c');
+
 
 }
